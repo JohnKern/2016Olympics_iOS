d68cfa7dfda1a5ca210c26499df2b5ae52a691d0
张可 <zhangke@zhangkeeMacBook.lan>
2015-10-28 07:09:30 +0800

    first commit

---
+++ 2016Olympics/2016Olympics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2016Olympics
+//
+//  Created by 张可 on 15/10/26.
+//  Copyright © 2015年 张可. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2016Olympics/2016OlympicsTests/_016OlympicsTests.m
@@ -0,0 +1,39 @@
+//
+//  _016OlympicsTests.m
+//  2016OlympicsTests
+//
+//  Created by 张可 on 15/10/26.
+//  Copyright © 2015年 张可. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _016OlympicsTests : XCTestCase
+
+@end
+
+@implementation _016OlympicsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
